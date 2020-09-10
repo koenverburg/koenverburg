c5e1272b59e950d053ad9d39048c777cef146432
Koen Verburg <creativekoen@gmail.com>
2020-09-10 09:24:59 +0200

    refactor(abstracted structs): import the structs and update the usings

--- src/shields/socials.go
+++ src/shields/socials.go
@@ -5,24 +5,12 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+
+	"github.com/koenverburg/koenverburg/src/types"
 )
 
-type SocialNodes struct {
-	Socials []Social `json:"data"`
-}
 
-type Social struct {
-	Style      string `json: "style"`
-	Platform   string `json: "platform"`
-	Logo       string `json: "logo"`
-	LogoColor  string `json: "logoColor"`
-	LabelColor string `json: "labelColor"`
-	IconColor  string `json: "iconColor"`
-	Handle     string `json: "handle"`
-	Link       string `json: "link"`
-}
-
-func createShieldURL(s Social) string {
+func createShieldURL(s types.Item) string {
 	formattedString := "https://img.shields.io/badge/-%s-%s?style=%s&labelColor=%s&logo=%s&logoColor=%s&link=%s"
 	result := fmt.Sprintf(formattedString, s.Handle, s.IconColor, s.Style, s.LabelColor, s.Logo, s.LogoColor, s.Link)
 	markdownImage := fmt.Sprintf("[![%s Badge](%s)](%s)", s.Platform, result, s.Link)
@@ -42,11 +30,11 @@ func PrepareSocialShields(file string) []string {
 	defer jsonFile.Close()
 
 	byteValue, _ := ioutil.ReadAll(jsonFile)
-	var socials SocialNodes
+	var socials types.List
 	json.Unmarshal(byteValue, &socials)
 
-	for i := 0; i < len(socials.Socials); i++ {
-		shield := createShieldURL(socials.Socials[i])
+	for i := 0; i < len(socials.Data); i++ {
+		shield := createShieldURL(socials.Data[i])
 		mkImage = append(mkImage, shield)
 	}
 
