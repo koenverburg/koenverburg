9e4db1e4f4cdf1dd964456cb3466c85221a47d89
Koen Verburg <creativekoen@gmail.com>
2020-09-10 17:12:07 +0200

    refactor: renamed backend to stack

--- src/shields/stack.go
+++ src/shields/stack.go
@@ -9,19 +9,18 @@ import (
 	"github.com/koenverburg/koenverburg/src/types"
 )
 
-// https://img.shields.io/badge/-{iconColor}?style={style}&labelColor={labelColor}&logo={logo}&logoColor={logoColor}&label={label}
-
 func createShieldURL2(s types.Item) string {
 	formattedString := "https://img.shields.io/badge/-%s?style=%s&labelColor=%s&logo=%s&logoColor=%s&label=%s"
 	result := fmt.Sprintf(formattedString, s.IconColor, s.Style, s.LabelColor, s.Logo, s.LogoColor, s.Label)
-	markdownImage := fmt.Sprintf("![%s Badge](%s)]", s.Label, result)
+	markdownImage := fmt.Sprintf("![%s Badge](%s)", s.Label, result)
 
 	return markdownImage
 }
 
-func PrepareBackendShields(file string) []string {
-	mkImage := make([]string, 4)
+func PrepareStackShields(file string) []string {
+	mkImage := make([]string, 4) // make this an dynamic array
 
+	// abstract this to an util func
 	jsonFile, err := os.Open(file)
 	if err != nil {
 		fmt.Println(err)
@@ -34,11 +33,12 @@ func PrepareBackendShields(file string) []string {
 
 	var list types.List
 	json.Unmarshal(byteValue, &list)
+	// end - abstract this to an util func
 
 	for i := 0; i < len(list.Data); i++ {
-		shield := createShieldURL2(list.Data[i])
+		shield := createShieldURL2(list.Data[i]) // make this an func that is passed along
 		mkImage = append(mkImage, shield)
 	}
 
-	return mkImage
+	return mkImage // join the array here?
 }
