bb249243fa920911d72525e9185228140868e529
Koen Verburg <creativekoen@gmail.com>
2020-09-10 17:13:36 +0200

    feat: hook up data with writing out the readme

--- src/main.go
+++ src/main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+	"io"
+	"os"
 	"fmt"
 
-	"github.com/koenverburg/readme/shields"
+	"github.com/koenverburg/koenverburg/src/utils"
+	"github.com/koenverburg/koenverburg/src/shields"
 )
 
 func main() {
 	fmt.Println("Started")
 
-	// socialShields := shields.PrepareSocialShields("../data/socials.json")
-	backendShields := shields.PrepareBackendShields("../data/stack.backend.json")
-	fmt.Println(backendShields)
+	socialShields := shields.PrepareSocialShields("../data/socials.json")
+	devopsShields := shields.PrepareStackShields("../data/stack.devops.json")
+	backendShields := shields.PrepareStackShields("../data/stack.backend.json")
+	frontendShields := shields.PrepareStackShields("../data/stack.frontend.json")
+
+	template := utils.CreateTemplate(socialShields, frontendShields, backendShields, devopsShields)
+
+	file, err := os.Create("../readme.md")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	_, err = io.WriteString(file, template)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	file.Sync()
 }
